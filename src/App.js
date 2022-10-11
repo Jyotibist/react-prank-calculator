d41c0b43386c7756524fa8f5f52736bc74a1b05d
Jyoti Bist~ <jyotibist9849@gmail.com>
2022-10-11 14:31:30 +1100

    calculator done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState } from 'react';
+import './App.css';
+import { BtnArea } from './components/BtnArea';
+import { Display } from './components/Display';
+const operators = ['+','-','*','/','%']
+
+const  App = () => {
+  const [strToDisplay, setStringToDisplay]= useState("");
+  const[lastOperator, setLastOperator]= useState("");
+
+  const handleOnButtonClick =(val)=>{
+    console.log(val);
+    if(val === "C"){
+      // const str = strToDisplay.slice(0, -1);
+      return setStringToDisplay(strToDisplay.slice(0, -1));
+    }
+    if(val === "AC"){
+      return setStringToDisplay("");
+      // console.log("reset display content");
+    }
+    if(val === "="){
+      const lastChar = strToDisplay[strToDisplay.length-1];
+      let temStr = strToDisplay;
+      if(operators.includes(lastChar)){
+        temStr = strToDisplay.slice(0,-1);
+      }
+      // const total = eval(strToDisplay).toString();
+      return setStringToDisplay(eval(temStr).toString());
+    }
+
+
+    if(operators.includes(val)){
+      console.log("you clicked operators")
+      if(!strToDisplay){
+          return;
+      }
+
+      let temStr = strToDisplay
+      const lastChar = strToDisplay[strToDisplay.length-1];
+      if(operators.includes(lastChar)){
+          temStr=strToDisplay.slice[0,-1];
+      }
+       setStringToDisplay(temStr+val);
+       setLastOperator(val);
+       return;
+    
+  } 
+
+   if (val === "."){
+    if(lastOperator){
+      const operatorIndex =  strToDisplay.lastIndexOf(lastOperator);
+      const numberAfterLastOperator = strToDisplay.slice(operatorIndex);
+      if(numberAfterLastOperator.includes(".")){
+        return;
+      }
+    }
+    if(!lastOperator && strToDisplay.includes(".")){
+      return;
+    }
+   }
+  
+    setStringToDisplay(strToDisplay + val);
+};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ="wrapper">
+      <div className="circle"></div>
+      <div className="calculator">
+        <Display strToDisplay = {strToDisplay}/>
+        <BtnArea handleOnButtonClick={handleOnButtonClick}/>
+      </div>
     </div>
   );
 }
